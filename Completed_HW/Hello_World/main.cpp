0fa99d73faa5438dd9880c32c7b829a41bf139f5
Error1417 <101829080+Error1417@users.noreply.github.com>
2023-11-01 18:36:38 -0700

    Add files via upload

---
+++ Completed_HW/Hello_World/main.cpp
@@ -0,0 +1,35 @@
+/* 
+ * Author: Ismael Perez 
+ * Date: 2/15/23
+ * Purpose: Hello world
+ */
+
+//System Libraries Here
+#include <iostream>
+
+using namespace std;
+
+//User Libraries Here
+
+//Global Constants Only, No Global Variables
+//Constants like PI, e, Gravity, Conversions, 2D array size only!
+
+//Function Prototypes Here
+
+//Program Execution Begins Here
+int main(int argc, char** argv) 
+{
+    //Set Random Number Seed Here
+    
+    //Declare all Variables Here
+    
+    //Input or initialize values Here
+    cout << "Hello World!  " << endl;
+    cout << endl;
+    //Process/Calculations Here
+    
+    //Output Located Here
+
+    //Exit
+    return 0;
+}
